b884d2a899f7871130ef2e690f51b931f188a86f
Arsen <xachatryanarsen63@gmail.com>
2026-05-14 21:11:53 +0400

    esim

--- classroom/react/src/main.jsx
+++ classroom/react/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./i18n";
+import { I18nextProvider } from "react-i18next";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <I18nextProvider>
     <App />
-  </StrictMode>,
-)
+  </I18nextProvider>
+ 
+);
