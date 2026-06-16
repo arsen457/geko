d25144691aa0fc1c1ee1ff1a322e4cb1b054e01d
Arsen <xachatryanarsen63@gmail.com>
2026-06-16 21:16:29 +0400

    gy

--- classroom/Python/script.py
+++ classroom/Python/script.py
@@ -1,2 +1,13 @@
-x = int(input("Enter your birthday "))
-print(f"your age is {2026-x}")
+while True:
+    try:
+        x = int(input("Gri tiv: "))
+        break
+    except ValueError: 
+        print("gri tiv voch te tar")
+
+if x < 0:
+    print("chkpav")
+elif x > 0:
+    print("exav")
+else:
+    print("zro")
